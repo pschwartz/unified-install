29aa0ae1bee51caa8999637df75163547de955b9
Philip Marc Schwartz <philip@progmad.com>
2010-11-08 15:30:16 -0500

    Added OptionParser class along with the usage of logging and logging.handler with curses to provide formated and colored output logging to stdout.

--- src/nova.py
+++ src/nova.py
@@ -5,25 +5,249 @@ Created on Nov 2, 2010
 
 import os
 import urllib2
+import logging
+import logging.handlers
+import re
+import sys
+import time
+
+try:
+    import curses
+except:
+    curses = None
 
 try:
     import simplejson as json
 except ImportError:
     import json
 
+class OptionParser(object):
+    class _CliOpts(dict):
+        @classmethod
+        def instance(cls):
+            if not hasattr(cls, "_instance"):
+                cls._instance = cls()
+            return cls._instance
+    
+        def __getattr__(self, name):
+            if isinstance(self.get(name), OptionParser._CliOpt):
+                return self[name].value()
+            raise AttributeError("Unrecognized option %r" % name)
+    
+    
+    class _CliOpt(object):
+        def __init__(self, name, default=None, type=str, help=None, metavar=None,
+                     multiple=False, file_name=None):
+            if default is None and multiple:
+                default = []
+            self.name = name
+            self.type = type
+            self.help = help
+            self.metavar = metavar
+            self.multiple = multiple
+            self.file_name = file_name
+            self.default = default
+            self._value = None
+    
+        def value(self):
+            if self._value is None:
+                return self.default
+            else:
+                return self._value
+    
+    
+        def parse(self, value):
+            _parse = {
+                bool: self._parse_bool,
+                str: self._parse_string,
+            }.get(self.type, self.type)
+            if self.multiple:
+                if self._value is None:
+                    self._value = []
+                for part in value.split(","):
+                    if self.type in (int, long):
+                        lo, _, hi = part.partition(":")
+                        lo = _parse(lo)
+                        hi = _parse(hi) if hi else lo
+                        self._value.extend(range(lo, hi+1))
+                    else:
+                        self._value.append(_parse(part))
+            else:
+                self._value = _parse(value)
+            return self.value()
+    
+        def set(self, value):
+            if self.multiple:
+                if not isinstance(value, list):
+                    raise OptionParser.Error("Option %r is required to be a list of %s" %
+                                (self.name, self.type.__name__))
+                for item in value:
+                    if item != None and not isinstance(item, self.type):
+                        raise OptionParser.Error("Option %r is required to be a list of %s" %
+                                    (self.name, self.type.__name__))
+            else:
+                if value != None and not isinstance(value, self.type):
+                    raise OptionParser.Error("Option %r is required to be a %s" %
+                                (self.name, self.type.__name__))
+            self._value = value
+    
+        
+        def _parse_bool(self, value):
+            return value.lower() not in ("false", "0", "f")
+    
+        def _parse_string(self, value):
+            return value.decode("utf-8")
+    
+    class Error(Exception):
+        pass
+    
+    class _LogFormatter(logging.Formatter):
+        def __init__(self, color, *args, **kwargs):
+            logging.Formatter.__init__(self, *args, **kwargs)
+            self._color = color
+            if color:
+                fg_color = curses.tigetstr("setaf") or curses.tigetstr("setf") or ""
+                self._colors = {
+                        logging.DEBUG: curses.tparm(fg_color, 4), # Blue
+                        logging.INFO: curses.tparm(fg_color, 2), # Green
+                        logging.WARNING: curses.tparm(fg_color, 3), # Yellow
+                        logging.ERROR: curses.tparm(fg_color, 1), # Red
+                    }
+                self._normal = curses.tigetstr("sgr0")
+        
+        def format(self, record):
+            try:
+                record.message = record.getMessage()
+            except Exception, e:
+                record.message = "Bad message (%r): %r" % (e, record.__dict__)
+            record.asctime = time.strftime(
+                    "%y-%m-%d %H:%M:%S", self.converter(record.created))
+            prefix = '[%(asctime)s %(module)s]' % \
+                    record.__dict__
+            if self._color:
+                prefix = (self._colors.get(record.levelno, self._normal) +
+                              prefix + self._normal)
+            formatted = prefix + " " + record.message
+            if record.exc_info:
+                if not record.exc_text:
+                    record.exc_text = self.formatException(record.exc_info)
+            if record.exc_text:
+                formatted = formatted.rstrip() + "\n" + record.exc_text
+            return formatted.replace("\n", "\n    ")
+    
+    @classmethod
+    def instance(cls, params=None):
+        if not hasattr(cls, "_instance"):
+            if params is None:
+                cls._instance = cls()
+            else:
+                cls._instance = cls(params)
+        return cls._instance
+    
+    def __init__(self, params=None):
+        self.options = OptionParser._CliOpts.instance()
+        self.params = params
+    
+    def option(self,name, default=None, type=str, help=None, metavar=None,
+           multiple=False):
+        if name in self.options:
+            raise OptionParser.Error("Option %r already defined in %s", name,
+                        self.options[name].file_name)
+        frame = sys._getframe(0)
+        options_file = frame.f_code.co_filename
+        file_name = frame.f_back.f_code.co_filename
+        if file_name == options_file: file_name = ""
+        self.options[name] = OptionParser._CliOpt(name, file_name=file_name, default=default,
+                                type=type, help=help, metavar=metavar,
+                                multiple=multiple)
+    
+    
+    def parse_command_line(self,args=None):
+        if args is None: args = sys.argv
+        remaining = []
+        for i in xrange(1, len(args)):
+            if not args[i].startswith("-"):
+                remaining = args[i:]
+                break
+            if args[i] == "--":
+                remaining = args[i+1:]
+                break
+            arg = args[i].lstrip("-")
+            name, equals, value = arg.partition("=")
+            name = name.replace('-', '_')
+            if not name in self.options:
+                self.print_help()
+                raise OptionParser.Error('Unrecognized command line option: %r' % name)
+            option = self.options[name]
+            if not equals:
+                if option.type == bool:
+                    value = "true"
+                else:
+                    raise OptionParser.Error('Option %r requires a value' % name)
+            option.parse(value)
+        if self.options.help:
+            self.print_help()
+            sys.exit(0)
+    
+        if self.options.logging != 'none':
+            logging.getLogger().setLevel(getattr(logging, self.options.logging.upper()))
+            self.enable_pretty_logging()
+    
+        return remaining   
+    
+    def parse_config_file(self,path):
+        config = {}
+        execfile(path, config, config)
+        for name in config:
+            if name in self.options:
+                self.options[name].set(config[name])
+    
+    def print_help(self,file=sys.stdout):
+        if self.params is not None:
+            print >> file, "Usage: %s [OPTIONS] %s" % (sys.argv[0],self.params)
+        else:
+            print >> file, "Usage: %s [OPTIONS]" % sys.argv[0]
+        print >> file, ""
+        print >> file, "Options:"
+        by_file = {}
+        for option in self.options.itervalues():
+            by_file.setdefault(option.file_name, []).append(option)
+    
+        for filename, o in sorted(by_file.items()):
+            if filename: print >> file, filename
+            o.sort(key=lambda option: option.name)
+            for option in o:
+                prefix = option.name
+                if option.metavar:
+                    prefix += "=" + option.metavar
+                print >> file, "  --%-30s %s" % (prefix, option.help or "")
+        print >> file
+        
+    def enable_pretty_logging(self):
+        root_logger = logging.getLogger()
+        color = False
+        if curses and sys.stderr.isatty():
+            try:
+                curses.setupterm()
+                if curses.tigetnum("colors") > 0:
+                    color = True
+            except:
+                pass
+        channel = logging.StreamHandler()
+        channel.setFormatter(OptionParser._LogFormatter(color=color))
+        root_logger.addHandler(channel)
+
 class ManifestImporter:
     def __init__(self, distribution="ubuntu", release="10.04", 
-                 host="github.com/piken/unified-install/raw/master/src", ssl=False, 
+                 host="https://github.com/piken/unified-install/raw/master/src", 
                  global_manifest="manifest.global"):
         self.distribution = distribution
         self.release = release
-        if ssl:
-            self.host = "https://"+ host + "/"
-        else:
-            self.host = "http://"+ host + "/"
+        self.host = host + "/"
         self.__VerifyGlobalManifest(manifest=global_manifest)
         
     def __VerifyGlobalManifest(self, manifest):
+        logging.info("Retrieving "+ self.host+manifest)
         self.global_manifest = json.load(urllib2.urlopen(self.host+manifest))
         if self.distribution in self.global_manifest:
             distro = self.global_manifest[self.distribution]
@@ -62,29 +286,45 @@ class ManifestImporterError(Exception):
             return message + " ("+ (self.log_message % self.args) +")"
         else:
             return message
-
-def usage():
-    print "nova-install [options] [distribution:release]"
-    print "Options:"
-    print "    --with-mysql                                        - Use MySQL with Nova"
-    print "    --mysql-user=[username] - Default: nova             - Set the MySQL Username"
-    print "    --mysql-password=[password] - Default: nova         - Set the MySQL Password"
-    print "    --network-interface=[interface] - Default: eth0     - Set the Network Interface" 
-    print "    --with-ldap                                         - Use LDAP Authentication"
-    print "    --branch=[branch]                                   - Set a Launchpad Nova Branch to use" 
-    print "    --manifest-host=[host]                              - Set host name to retrieve manifests from"
-    print "    --libvirt-type=[type] - Default: qemu               - Set libvirt-type to use"
-    print "    --test-install                                      - Run tests after install"
-
         
 def main():
-    usage()
-    mi = ManifestImporter(ssl=True)
+    opt.parse_command_line()
+    mi = ManifestImporter()
     manifest = mi.ManifestImport()
     for i in manifest["dependencies"]["PckMgr"]:
-        print i,manifest["dependencies"]["PckMgr"][i]
+        logging.info("%s %s" % (i,manifest["dependencies"]["PckMgr"][i]))
         
-    
+
     
 if __name__ == '__main__':
+    opt = OptionParser.instance("[distribution:release]")
+    opt.option("help", type=bool, help="Show this help information")
+    opt.option("logging", default="info",
+               help=("Set the Python log level."),
+               metavar="info|warning|error")
+    opt.option("with-mysql", type=bool, help="Use MySQL with Nova")
+    opt.option("with-mysql-host", default="localhost", type=str, 
+               help="Set the MySQL Hostname - Default:localhost",
+               metavar="hostname")
+    opt.option("with-mysql-user", default="nova", type=str, 
+               help="Set the MySQL Username - Default:nova",
+               metavar="username")
+    opt.option("with-mysql-password", default="nova" ,type=str, 
+               help="Set the MySQL Password - Default:nova",
+               metavar="password")
+    opt.option("network-interface", default="eth0",type=str, 
+               help="Set the Network Interface - Deafult:eth0",
+               metavar="interface")
+    opt.option("with-ldap",type=bool, help="Use LDAP Authentication")
+    opt.option("use-branch", default="lp:nova", type=str, 
+               help="Set a Nova Branch to use - Deafult: lp:nova",
+               metavar="branch")
+    opt.option("manifest-host", 
+               default="https://github.com/piken/unified-install/raw/master/src",
+               type=str, help="Set host for manifest retrieval",
+               metavar="host")
+    opt.option("libvirt-type", default="qemu", type=str, 
+               help="Set libvirt-type to use - Default:qemu",
+               metavar="qemu|xen|uml")
+    opt.option("test-install",type=bool, help="Run tests after install")
     main()
